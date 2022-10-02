489c25977171e62ec9bcc0bef28baccc748096c6
Mukola <koliunia2006@gmail.com>
2022-10-02 16:23:06 +0300

    homework-1

---
+++ app.js
@@ -0,0 +1,41 @@
+// 4. Зміни ім"я товара на "Repair droid" і збільшіть його ціну
+    // на 1500 кредитів. Переопреділіть значення змінних pricePerItem і productName
+    // після їх об"явлення.
+    // let productName = "Droid";
+    // let pricePerItem = 2000;
+
+    //  let productName = "Droid";
+    //  let pricePerItem = 2000;
+    //  productName = "Repair droid";
+    //  pricePerItem = 3500;
+    //  console.log();
+
+     // 5. Об"явіть змінні використавуючи ключові слова const та let та присвойте
+    // їм відповідні значення.
+    // topSpeed - число 160.
+    // distance - число 617.54.
+    // login - рядок "mango935".
+    // isOnline - буль true.
+    // isAdmin - буль false.
+    // let topSpeed = 160;
+    // let distance = 617.54;
+    // const login = "mango935";
+    // const isOnline = true;
+    // const isAdmin = true;
+
+    // 6. Доповни код, надавши змінній totalPrice вираз для підрахунку загальної суми 
+    // замовлення. Змінна pricePerItem зберігає ціну однієї одиниці товару, 
+    // а orderedQuantity - кількість одиниць товару в замовлення.
+    // const pricePerItem = 3500;
+    // const orderedQuantity = 4;
+
+    // const pricePerItem = 3500;
+    // const orderedQuantity = 4;
+    // const totalPrice = pricePerItem * orderedQuantity;
+
+    // 12. Створіть змінну в якій буде зберігатися псевдовипадкове число від 0 до 1 та зміни діапазон від 50 до 100.
+    // let randomNumber = Math.random();
+    // randomNumber = Math.random() * (100 - 50) + 50;
+    // console.log(Math.roundrandomNumber);
+    // 11. Напиши скрип який буде збільшувати заробітню платню робітника на 10%. Початкова сума зп - 25000.589612 грн. Результат округли до сотої та виведи в консоль.
+
---
+++ homework-1.js
@@ -0,0 +1,89 @@
+// ДЗ 1
+// Part 1
+const str = 'Привіт';
+const num = 123;
+const flag = true;
+const txt = 'true';
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+const room = 17;
+const age = 20;
+const name = 'Nick';
+const surname = 'Salamakha';
+const hotel = 'Coin';
+
+let o = 15;
+let t = 10;
+let c = Number('51');
+let mood = 'happy';
+let value = 9;
+
+//Part 2
+// Завдання 1
+const yourName = prompt('Як вас звати?')
+console.log(yourName);
+
+// Завдання 2
+const ma = 15;
+const fn = 14;
+alert(ma + fn);
+
+// Завдання 3
+let a = 10;
+let b = 10;
+console.log(a + b);
+
+// Завдання 4
+let a1 = 5 + 3;
+console.log(a1);
+
+let a2 = 5 - 3;
+console.log(a2);
+
+let a3 = 5 * 3;
+console.log(a3);
+
+let a4 = 5 / 3;
+console.log(a4);
+
+let a5 = 5 + 4 / 2;
+console.log(a5);
+
+// Завдання 5
+const a6 = 5 % 3;
+const a7 = 3 % 5;
+const a8 = 5 + '3';
+const a9 = '5' - 3;
+const a10 = 75 + 'кг';
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+
+// Завдання 6
+let height = 23;
+let width = 10;
+let s = 230;
+
+// Завдання 7
+const heightC = 10;
+const dC = 4 ;
+const v = 40;
+
+// Завдання 8
+const n = 3;
+const m = 4;
+const k = (n**2)+ (m**2);
+console.log(k);
+
+
+
+
+
+
